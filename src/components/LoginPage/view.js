4694db1002a7192ba570853c638c824d79e0d048
Artyom Furmanov <Artyom.Furmanov@itechart-group.com>
2019-06-05 17:15:19 +0300

    fix: login view
    rewrite component as pure func

--- src/components/LoginPage/view.js
+++ src/components/LoginPage/view.js
@@ -1,34 +1,22 @@
 /* eslint-disable react/no-unused-state */
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Login } from './LoginForm';
 
-export const fakeValidEmail = 'vidgf@sdf.sfn';
-export const fakeValidPassword = '12345QWE';
-
-class LoginPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogged: false
-    };
-  }
-
-  render() {
-    const { isFailed } = this.props;
-    return (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%'
-        }}
-      >
-        <Login isFailed={isFailed} />
-      </div>
-    );
-  }
+function LoginPage(props) {
+  const { isFailed } = props;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%'
+      }}
+    >
+      <Login isFailed={isFailed} />
+    </div>
+  );
 }
 
 LoginPage.propTypes = {
